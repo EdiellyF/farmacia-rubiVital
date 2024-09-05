ab9538f767fda1eb9eadbad7161e4e15cf50c677
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2024-09-05 10:25:56 -0300

    fix:vercel add floder api

--- api/carrosel.php
+++ api/carrosel.php
@@ -1,12 +1,12 @@
 <div class="relative w-full max-w-4xl">
         <div class="carousel-item active">
-            <img src="/src/imgs/imgs-carrosel/rubi.jpg" alt="Imagem 1" class="w-full lg:h-16 object-cover">
+            <img src="../src/imgs/imgs-carrosel/rubi.jpg" alt="Imagem 1" class="w-full lg:h-16 object-cover">
         </div>
         <div class="carousel-item">
-            <img src="/src/imgs/imgs-carrosel/img-1.jpg" alt="Imagem 2" class="w-full h-32 ">
+            <img src="../src/imgs/imgs-carrosel/img-2.jpg" alt="Imagem 2" class="w-full h-32 ">
         </div>
         <div class="carousel-item">
-            <img src="./src/imgs/imgs-carrosel/img-2.jpg" alt="Imagem 3" class="w-full object-cover">
+            <img src="../src/imgs/imgs-carrosel/img-1.jpg" alt="Imagem 3" class="w-full object-cover">
         </div>
 
         <div class="flex justify-center m-8 p-32">      
---
+++ api/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/src/index.css">
+    <link href="/src/output.css"rel="stylesheet">
+    <link rel="stylesheet" href="">
+</head>
+<body class="">
+    
+    <div id="header">
+            <header>
+            <?php include 'topo.php'; ?>
+            </header>
+
+            <section class="/flex justify-center  ">
+            <?php include 'carrosel.php'; ?>
+            </section>
+       
+
+            <main class="container-div">
+                    <div id="produtos">
+                        <div class="produto"><img src="../src/imgs/produto1.png" alt="Produto 1"></div>
+                        <div class="produto"><img src="../src/imgs/produto2.png" alt="Produto 2"></div>
+                        <div class="produto"><img src="../src/imgs/produto3.png" alt="Produto 3"></div>
+                        <div class="produto"><img src="../src/imgs/absorvete.png" alt="Produto 4"></div>
+                    </div>
+            </main>
+                
+                
+
+            
+  
+        <script type="module" src="../src/js/main.js"></script>
+  
+</body>
+</html>
